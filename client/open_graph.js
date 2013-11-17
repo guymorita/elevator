1ab7a3a8a5918b3123d2d90b99d9fb9d2158e7e6
Shaohua Zhou <shao@versal.com>
2013-11-16 20:06:42 -0800

    Getting current.json

--- client/open_graph.js
+++ client/open_graph.js
@@ -4,13 +4,16 @@ Template.open_graph.events({
     yam.getLoginStatus(function(res){
       if(res.authResponse){
         console.log("logged in");
+
+        var payload = {    //use the data object literal to specify parameters, as documented in the REST API section of this developer site
+          "letter": "a",
+          "page": "2",
+        };
+
         yam.request({
-          url: "https://www.yammer.com/api/v1/users.json",     //this is one of many REST endpoints that are available
+          url: "https://www.yammer.com/api/v1/networks/current.json",     //this is one of many REST endpoints that are available
           method: "GET",
-          data: {    //use the data object literal to specify parameters, as documented in the REST API section of this developer site
-            "letter": "a",
-            "page": "2",
-          },
+          // data: payload,
           success: function (user) { //print message response information to the console
             console.log("The request was successful.");
             console.log(user);
