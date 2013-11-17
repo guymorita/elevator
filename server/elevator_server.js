8f915d659438a57efe67db07e74f7ca39e233145
Shaohua Zhou <shao@versal.com>
2013-11-16 17:51:45 -0800

    Add all user obj

--- server/elevator_server.js
+++ server/elevator_server.js
@@ -13,8 +13,22 @@ Meteor.methods({
   increment_counter: function(competition, value){
 
   },
-  new_user: function(userid){
+  new_user: function(input){
+    if(!input){
+      return null;
+    }
 
+    var user_obj = {};
+    //try getting yammer_id
+    user_obj.yammer_id = '' + input.user.id;
+    _.extend(user_obj, input);
+
+    Users.upsert({yammer_id:user_obj.yammer_id}, user_obj, function(error, result){
+      console.log('error', error);
+      console.log('result', result);
+    });
+
+    return user_obj.yammer_id;
   },
   add_user_to_competition: function(competitionid, userId){
 
