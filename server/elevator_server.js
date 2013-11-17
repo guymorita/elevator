887d61429c439e066a93ef21f9b5523e76db030c
Guy Morita <guy@hipmunk.com>
2013-11-16 17:53:11 -0800

    DB changes

--- server/elevator_server.js
+++ server/elevator_server.js
@@ -4,20 +4,23 @@ Competitions = new Meteor.Collection("competitions");
 Meteor.startup(function () {
 
 });
+
 Meteor.methods({
   new_competition: function(competitionObj){
     return Competition.insert(competitionObj, function(competitionId) {
       return competitionId;
     });
   },
-  increment_counter: function(competition, userId, value){
-
+  increment_counter: function(competitionId, userId, value){
+    // Competition.update({_id: competitionId}, {users: }
   },
   new_user: function(userid){
 
   },
   add_user_to_competition: function(competitionid, userId){
-
+    return Competition.update({_id: competitionid}, {$push: {userId: userId}}, function(err){
+      return err;
+    });
   },
   get_competition_object: function(competitionId){
     return Competition.find({competitionId: competitionId});
@@ -25,5 +28,4 @@ Meteor.methods({
   get_all_competitions: function(){
     return Competition.find({});
   }
-
 });
