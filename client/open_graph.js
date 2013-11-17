fc3e63abb3eb4e91e52433b98bf72c342dd3af7b
Shaohua Zhou <shao@versal.com>
2013-11-16 19:43:22 -0800

    WIP for open graph

--- client/open_graph.js
+++ client/open_graph.js
@@ -2,7 +2,9 @@ Template.open_graph.events({
   'click .post' : function () {
     console.log('post');
     yam.getLoginStatus(function(res){
-      console.log('res', res);
+      if(res.authResponse){
+        console.log('res', res);
+      }
     });
   }
 });
