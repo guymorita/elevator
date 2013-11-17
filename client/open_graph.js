f783dc0ac983eef48847e0afb694bda1ff125ad8
Shaohua Zhou <shao@versal.com>
2013-11-16 20:02:54 -0800

    Follow basic examples

--- client/open_graph.js
+++ client/open_graph.js
@@ -3,7 +3,24 @@ Template.open_graph.events({
     console.log('post');
     yam.getLoginStatus(function(res){
       if(res.authResponse){
-        console.log('res', res);
+        console.log("logged in");
+        yam.request({
+          url: "https://www.yammer.com/api/v1/users.json",     //this is one of many REST endpoints that are available
+          method: "GET",
+          data: {    //use the data object literal to specify parameters, as documented in the REST API section of this developer site
+            "letter": "a",
+            "page": "2",
+          },
+          success: function (user) { //print message response information to the console
+            console.log("The request was successful.");
+            console.log(user);
+          },
+          error: function (user) {
+            console.log("There was an error with the request.");
+          }
+        });
+      } else {
+        console.log('logged out');
       }
     });
   }
